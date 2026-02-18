e3fa3fd5eb0cf6c39023b8bd3412a653fde7175a
divyabansal20-ag <divyabansalthal@gmail.com>
2026-02-18 13:08:27 +0530

    pattern

---
+++ Pattern.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Pattern{
+    public static void main(String[] args){
+      Scanner s = new Scanner(System.in);
+      int a = s.nextInt();
+      for(int i = 0;i<=a;i++){
+         System.out.println("a");
+  }
+ }
+}
